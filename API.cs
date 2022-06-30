140ac6a717d6fe97ba0ecd74acf478646889e702
walumo <miikka.hakulinen@awacademy.training>
2022-06-30 13:32:25 +0300

    Models to  folder; cleanup

--- API.cs
+++ API.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Net;
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Linq;
-using System.Threading.Tasks;
 
 
 namespace ApiHelper
